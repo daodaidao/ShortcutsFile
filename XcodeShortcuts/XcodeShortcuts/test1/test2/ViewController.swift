7f3a10a7488901b83b3fd9e5f725330b6fc5b518
JayShang <yijieshang@gmail.com>
2018-05-31 17:01:53 +0800

    常用快捷键1整理

---
+++ XcodeShortcuts/XcodeShortcuts/test1/test2/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  XcodeShortcuts
+//
+//  Created by caihongguang on 2018/5/30.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+/*
+ 参考资料1:http://www.cocoachina.com/ios/20160822/17393.html
+ 
+ 
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        /* 1.更新约束.  等于号. (比如有个黄色，可以应用黄色)
+         command + option + =
+         
+         2.移动代码
+         command + option + [ 或者 ] 上下移动代码
+         command + [ / ] 左右移动代码
+         
+         3.展开/隐藏 代码
+          command + option + <-  /  ->  合并/展开代码块
+         
+         4.全局修改局部变量
+         command + control + e
+         
+         5.选中 Navigator
+         command + 1 2 3 4  (0 为隐藏)
+         注意不要设置其他工具快捷键和工具的冲突
+         
+         6.右上角属性面板
+         command + option + 1 2... (0 为隐藏)
+         //右下角控制面板
+         command + option + control + 1 2...
+         
+         7. storyboard 中快速打开对应的控制器
+         command + option + enter (实验结果 只适合sb,不能在xib上用)
+         
+         8.主控制面板
+         control + 1 2 3 ... 可以控制例如 XcodeShortcuts->xxx_>xxx 文件层级多了可以用
+         control + 6 弹出该类所有方法列表
+         
+         9.隐藏/打开控制台
+         command + shift + y
+         
+         10.快速打开全局查询
+         command + shift + f
+         
+         11.快速切换到 .m .h文件
+         command + control + up / down
+         
+         
+         12.快速切换到文件的顶部/底部
+         command + up / down
+         
+         13.删除光标所在行之前的代码
+         command + delete
+         
+         删除光标之前的一个单词
+         option + delete
+         
+         将光标移动刀所在行的最前面/后面
+         command + 左  / 右
+         
+         将光标移动到所在单词的最前面/后面
+         option + 左 / 右
+         
+         14.开启或取消当前行断点
+         command + \
+         开启或关闭全部断点
+         command + Y
+         
+         15. command + . 停止运行
+         
+         */
+        
+        var test = String()
+        test = "678"
+        
+        
+    }
+ 
+}
+
